ccafb83df5dec2d258e041fec95d775007241003
Alan R. Soares <alanrsoars@gmail.com>
2016-09-06 22:37:47 +1200

    switch store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import configureStore from './store/'
 import App from './containers/App'
 import './index.css'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
