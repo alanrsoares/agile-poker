12095470614dbb5b9b3706d6e4661f3d55cf42f7
Alan R. Soares <alanrsoars@gmail.com>
2016-09-09 13:38:27 +1200

    remove CheckAuth

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
-import actions from './actions'
-import * as selectors from './selectors';
-
 import AppLayout from './layouts/App'
 import App from './containers/App'
-
 import SignIn from './containers/SignIn'
 
 import configureStore from './store'
@@ -18,24 +14,18 @@ const store = configureStore()
 
 const history = syncHistoryWithStore(browserHistory, store)
 
-const checkAuth = store => (nextState, replace, callback) => {
-  if (!selectors.isUserLoggedIn(store.getState())) {
-    replace({
-			pathname: '/signin',
-			state: { nextPathname: nextState.location.pathname },
-		});
-  }
-  callback()
-}
-
-ReactDOM.render(
+const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={AppLayout}>
-        <IndexRoute component={App} onEnter={checkAuth(store)} />
+        <IndexRoute component={App} />
         <Route path="/signin" component={SignIn} />
       </Route>
     </Router>
-  </Provider>,
+  </Provider>
+)
+
+ReactDOM.render(
+  <Root store={store} />,
   document.getElementById('root')
 )
