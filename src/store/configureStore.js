afaa8b3b1263184137b729f11a5c0d2d541d719b
Alan R. Soares <alanrsoars@gmail.com>
2016-09-14 00:15:31 +1200

    one configureStore to rule them all

---
+++ src/store/configureStore.js
@@ -0,0 +1,34 @@
+import { createStore, compose, applyMiddleware } from 'redux'
+import { browserHistory } from 'react-router'
+import { routerMiddleware } from 'react-router-redux'
+import createSagaMiddleWare from 'redux-saga'
+
+import reducers from '../reducers'
+import rootSaga from '../sagas/index'
+
+const sagaMiddleware = createSagaMiddleWare()
+
+export default function configureStore(initialState) {
+	let middleware = applyMiddleware(
+		sagaMiddleware,
+		routerMiddleware(browserHistory)
+	)
+
+	if (process.env.NODE_ENV !== 'production') {
+    if (typeof window.devToolsExtension === 'function') {
+      middleware = compose(middleware, window.devToolsExtension());
+    }
+  }
+
+	const store = createStore(reducers, middleware, initialState)
+
+	sagaMiddleware.run(rootSaga)
+
+	if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      store.replaceReducer(require('../reducers').default);
+    });
+  }
+
+	return store
+}
